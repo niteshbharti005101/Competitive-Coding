112d2dea6f488f38e362c5446396d09862407c36
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2019-08-09 22:29:18 +0530

    bigSum.cpp

---
+++ iamshm/bigSum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int i, n;
+ 
+  long long int it, sum=0;
+  cin>> n;
+  for(i=0;i<n;i++)
+  {
+      cin>>it;
+      sum=sum+it;
+  }
+  cout<<sum;
+
+}
